050e077cfaf290f2008849e9892a9524fb99df3a
Zeeshan Raza <42145844+ZeeshanRaza25@users.noreply.github.com>
2020-08-31 22:16:39 +0500

    updated with pwa

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+console.log('registered!');
+let cacheData = 'appv1';
+// var urlsToCache = [
+//   '/',
+//   '/styles/main.css',
+//   '/script/main.js'
+// ];
+
+this.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        '/',
+        '/static/js/bundle.js',
+        '/static/js/1.chunk.js',
+        '/static/js/main.chunk.js',
+        '/manifest.json',
+        // '/favicon.ico',
+        'index.html',
+        'icon-192x192.png',
+        '/images/icons/icon-192x192.png',
+        // 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple',
+        // 'localhost',
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', (event) => {
+  // console.log('result');
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.match(event.request).then((result) => {
+        console.log('result', result);
+        if (result) {
+          console.log(result);
+          return result;
+        }
+      })
+    );
+  }
+});
